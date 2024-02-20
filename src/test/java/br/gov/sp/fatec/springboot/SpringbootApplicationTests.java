37f5237bf948da3d47279bbd5bfe3469e6757544
Marcelo <Marcelo>
2024-02-20 10:09:27 -0300

    Iniciando projeto

---
+++ src/test/java/br/gov/sp/fatec/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
