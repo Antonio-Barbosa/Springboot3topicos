8bfd07fe32dd2fd78dda532a5c84015f4935159c
Antonio Marcelo <antoniomarcelo545@gmail.com>
2024-03-04 13:30:16 +0000

    Mapeando autorizaçoes

--- src/main/java/br/gov/sp/fatec/springboot/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/springboot/entity/Usuario.java
@@ -1,10 +1,20 @@
 package br.gov.sp.fatec.springboot.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +31,27 @@ public class Usuario {
 
     @Column(name = "usr_senha")
     private String senha;
+ 
+    // nome do atributo que tem o join colomn na entidade Anotacao
+    @JsonIgnore
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+    private Set<Anotacao> anotacoes = new HashSet<Anotacao>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "uau_usuario_autorizacao",
+        joinColumns = {@JoinColumn(name = "usr_id")},
+        inverseJoinColumns = {@JoinColumn(name="aut_id")}
+    )
+    private Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
+
+    public Set<Autorizacao> getAutorizacoes() {
+        return autorizacoes;
+    }
+
+    public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
+        this.autorizacoes = autorizacoes;
+    }
 
     public Long getId() {
         return id;
@@ -45,5 +76,15 @@ public class Usuario {
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
+    public Set<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(Set<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+
     
+
 }
